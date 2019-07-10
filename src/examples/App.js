8e150237ef87351aa3a925317b8700f5bfa4bc34
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-10 07:42:29 +0100

    Chore: Create an example component to use for testing our published npm package

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { TextInput } from "../lib";
+import {ErrorPage} from 'react-custom-errors';
 
 const App = () => (
-  <div style={{ width: 640, margin: "15px auto" }}>
-    <h1>Hello React</h1>
-    <TextInput label="Email Address" placeholder="name@example.com" />
+  <div>
+    <ErrorPage code="404" />
   </div>
 );
 
