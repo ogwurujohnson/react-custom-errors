8188977eb3db81c780534e089a79f431757f4561
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-07-11 15:35:01 +0100

    Chore: Update package to current version

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import ErrorPage from '../lib/components/ErrorPage';
+import ErrorPage from 'react-custom-errors';
 
 
 const App = () => (
   <div>
-    <ErrorPage code={404} customMessage="" link="/yejhhhh" backgroundColor="" />
+    <ErrorPage
+      code={403}
+      customMessage="hfhfh"
+      link="/yejhhhh"
+      backgroundColor="brown"
+    />
   </div>
 );
 
